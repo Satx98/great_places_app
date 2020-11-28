1afa1ec1e75d9c12d0ffe00ee4b5e0fe11ef956b
Satadru Roy <satadru.roy98@gmail.com>
2020-11-28 22:56:39 +0530

    - Added the screen for adding places - Added ImageInput Field (widget) - Used Device Camera - Stored the image in the device

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await _storedImage.copy('${appDir.path}/$fileName');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey,
+              width: 1,
+            ),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage == null
+              ? Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                )
+              : Image.file(
+                  _storedImage,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: _takePicture,
+            icon: Icon(Icons.camera),
+            label: Text('Take Picture'),
+          ),
+        ),
+      ],
+    );
+  }
+}
