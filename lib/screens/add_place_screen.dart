6ed12b8515c384b769ede0a70b5a23d7c95fefa2
Satadru Roy <satadru@Satadrus-MacBook-Air.local>
2020-11-29 22:16:02 +0530

    - Worked on PlacesListScreen - Made Sure to save the title & image when AddPlace is saved

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -11,6 +15,25 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(
+      context,
+      listen: false,
+    ).addPlace(
+      _titleController.text,
+      _pickedImage,
+    );
+    Navigator.of(context).pop();
+  }
 
   @override
   void dispose() {
@@ -20,49 +43,51 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add a New Place'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: _titleController,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    ImageInput(),
-                  ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Add a New Place'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Title'),
+                        controller: _titleController,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      ImageInput(_selectImage),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          RaisedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-            label: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12.0),
-              child: Text(
-                'Add Place',
-                style: TextStyle(
-                  fontSize: 15,
+            RaisedButton.icon(
+              onPressed: _savePlace,
+              icon: Icon(Icons.add),
+              label: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: Text(
+                  'Add Place',
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
                 ),
               ),
+              elevation: 0,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              color: Theme.of(context).accentColor,
             ),
-            elevation: 0,
-            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            color: Theme.of(context).accentColor,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './add_place_screen.dart';
+import '../providers/great_places.dart';
 
 class PlacesListScreen extends StatelessWidget {
   static const routeName = '/places-list';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Places'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
-            },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Your Places'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+            ),
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+          child: Center(
+            child: Text(
+              'Got no places yet, start adding some!',
+              textAlign: TextAlign.center,
+            ),
           ),
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
+          builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+              ? ch
+              : Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: ListView.builder(
+                    itemBuilder: (ctx, i) => Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.items[i].image),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to Detail page...
+                          },
+                        ),
+                        Divider()
+                      ],
+                    ),
+                    itemCount: greatPlaces.items.length,
+                  ),
+                ),
+        ),
       ),
     );
   }
